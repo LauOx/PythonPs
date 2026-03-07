85ef3692896bc22f994935ea960c0e98c13fb13a
LauOx <laulefu@gmail.com>
2026-03-07 14:37:10 +0100

    ex3 terminado

--- m03/ex2/ft_coordinate_system.py
+++ m03/ex2/ft_coordinate_system.py
@@ -2,37 +2,58 @@ import sys
 import math
 
 
+def check_arg_len(arguments: list) -> None:
+    """
+    checks how many arguemnts entered
+    """
+    if len(arguments) != 3:
+        raise ValueError("Coordinate system needs 3 values "
+                         f"(entered {len(arguments)})")
+
+
+def compute_distance(arguments: str) -> None:
+    """
+    calculates the distance between two 3D points,
+    using the Euclidean distance formula
+    """
+    # change str to tuple
+    x2 = int(arguments[0])
+    y2 = int(arguments[1])
+    z2 = int(arguments[2])
+    coordinates = (x2, y2, z2)
+    # define tuple start point
+    start_point = (0, 0, 0)
+    x1, y1, z1 = start_point
+    # math
+    distance = float(math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2))
+    print(f"Position created: {coordinates}")
+    print(f"Distance between {start_point} and {coordinates}: "
+          f"{distance:.2f}")
+    print()
+    # demonstration
+    print("Unpacking demonstration:")
+    print(f"Player at x={x2}, y={y2}, z={z2}")
+    print(f"Coordinates at X={x2}, Y={y2}, Z={z2}")
+
+
 def coordinate_system():
     """
     """
     print("=== Game Coordinate System ===\n")
     ar_len = len(sys.argv)
     error_tracker = False
-    # define parsing if arg entered as "str"
+    # define parsing if arg entered as "str" and checks errors
     if ar_len == 2:
-        parsing = sys.argv[1].split()
+        arguments = sys.argv[1].split()
         try:
-            if len(parsing) != 3:
-                raise ValueError("Coordinate system needs 3 values "
-                                 f"(entered {len(parsing)})")
-            try:
-                coor = f'"{parsing[0]},{parsing[1]},{parsing[2]}"'
-                for x in parsing:
-                    int(x)
-                print(f'Parsing coordinates: {coor}')
-            except ValueError as e:
-                error_tracker = True
-                print(f"Parsing invalid coordinates: {coor}")
-                print(f"Error parsing coordinates: {e}")
-                print('Error details - Type: ValueError, '
-                      f'Args ("{e}".)')
+            check_arg_len(arguments)
         except ValueError as e:
             error_tracker = True
             print(f"Caught ValueError: {e}")
     # define parsing if entered 3 values
     elif len(sys.argv) == 4:
-        parsing = sys.argv[1:]
-    # raise error and end program if number of values is incorrect
+        arguments = sys.argv[1:]
+    # raise error and if number of arguments is incorrect
     else:
         try:
             raise ValueError("coordinate system needs 3 values "
@@ -40,36 +61,23 @@ def coordinate_system():
         except ValueError as e:
             error_tracker = True
             print(f"Caught ValueError: {e}")
-    # parsing is defined
+    # checks all arguments are int
     if not error_tracker:
         try:
-            coor = f'"{parsing[0]},{parsing[1]},{parsing[2]}"'
-            for x in parsing:
+            coor = f'"{arguments[0]},{arguments[1]},{arguments[2]}"'
+            for x in arguments:
                 int(x)
+            if ar_len == 2:
+                print(f'Parsing coordinates: {coor}')
         except ValueError as e:
             error_tracker = True
             print(f"Parsing invalid coordinates: {coor}")
             print(f"Error parsing coordinates: {e}")
             print('Error details - Type: ValueError, '
                   f'Args ("{e}".)')
-    # create tuple coordinates
+    # compute distance
     if not error_tracker:
-        x2 = int(parsing[0])
-        y2 = int(parsing[1])
-        z2 = int(parsing[2])
-        coordinates = (x2, y2, z2)
-        # define start point
-        start_point = (0, 0, 0)
-        x1, y1, z1 = start_point
-        distance = float(math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2))
-        print(f"Position created: {coordinates}")
-        print(f"Distance between {start_point} and {coordinates}: "
-              f"{distance:.2f}")
-        print()
-        # demonstration
-        print("Unpacking demonstration:")
-        print(f"Player at x={x2}, y={y2}, z={z2}")
-        print(f"Coordinates at X={x2}, Y={y2}, Z={z2}")
+        compute_distance(arguments)
 
 
 if __name__ == "__main__":

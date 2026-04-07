ee68582f3ddb20bb3a37d3b6538ef3a1838f2b95
LauOx <laulefu@gmail.com>
2026-04-07 13:23:24 +0200

    m05

---
+++ m05/ex0/data_processor.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+from typing import Any, List, Tuple, Union, Dict
+
+class DataProcessor(ABC):
+    pass
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        """"""
+
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        """"""
+
+    def output(self) -> Tuple[int, str]:
+        """"""
+        x = self.rank
+        self.remaining = self.added
+        i = 0
+        while i < len(self.data_stored) and self.data_stored[i] != ",":
+            i += 1
+        y = self.data_stored[:i]
+        self.data_stored = self.data_stored[i + 2:]
+        self.remaining -= 1
+        self.rank += 1
+        out = x, y
+        return out
+
+
+class NumericProcessor(DataProcessor):
+    def __init__(self): 
+        """"""
+        self.data_stored = ""
+        self.rank = 0
+        self.added = 0
+        self.remaining = 0
+
+    def validate(self, data: Any) -> bool:
+        """"""
+        value = False
+        if isinstance(data, (int, float)):
+            value = True
+            return value
+        elif isinstance(data, (list)):
+            return all(isinstance(d, (int, float)) for d in data)
+        return value
+
+    def ingest(self, data: Union[int, float, List]) -> None:
+        """"""
+        try:
+            if self.validate(data):
+                if isinstance(data, (int, float)):
+                    self.data_stored += str(data)
+                    self.added += 1
+                    self.remaining += 1
+                elif isinstance(data, list):
+                    for d in data:
+                        if not self.data_stored:
+                            self.data_stored += str(d)
+                            self.added += 1
+                            self.remaining += 1
+                        else:
+                            self.data_stored += ", " + str(d)
+                            self.added += 1
+                            self.remaining += 1
+            else:
+                raise ValueError("Improper numeric data")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+class TextProcessor(DataProcessor):
+    def __init__(self): 
+        """"""
+        self.data_stored = ""
+        self.rank = 0
+        self.remaining = 0
+        self.added = 0
+
+    def validate(self, data: Any) -> bool:
+        """"""
+        value = False
+        if isinstance(data, (str)):
+            value = True
+            return value
+        elif isinstance(data, (list)):
+            return all(isinstance(d, (str)) for d in data)
+        return value
+    
+    def ingest(self, data: Union[str, List]) -> None:
+        """"""
+        try:
+            if self.validate(data):
+                if isinstance(data, (str)):
+                    self.data_stored += data
+                    self.added += 1
+                    self.remaining += 1
+                elif isinstance(data, list):
+                    for d in data:
+                        if not self.data_stored:
+                            self.data_stored += d
+                            self.added += 1
+                            self.remaining += 1
+                        else:
+                            self.data_stored += ", " + d
+                            self.added += 1
+                            self.remaining += 1
+            else:
+                raise ValueError("Improper string data")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+class LogProcessor(DataProcessor):
+    def __init__(self): 
+        """"""
+        self.data_stored = ""
+        self.rank = 0
+        self.remaining = 0
+        self.added = 0
+
+    def validate(self, data: Any) -> bool:
+        """"""
+        value = False
+        if isinstance(data, (Dict)):
+            value = True
+            return value
+        elif isinstance(data, (list)):
+            return all(isinstance(d, (Dict)) for d in data)
+        return value
+    
+    def ingest(self, data: Union[Dict, List]) -> None:
+        """"""
+        try:
+            if self.validate(data):
+                if isinstance(data, (Dict)):
+                    level = data['log_level']
+                    message = data["log_message"]
+                    self.data_stored += f"{level}: {message}"
+                    self.added += 1
+                    self.remaining += 1
+                elif isinstance(data, list):
+                    i = 0
+                    for d in data:
+                        if not self.data_stored:
+                            level = data[i]['log_level']
+                            message = data[i]["log_message"]
+                            self.data_stored += f"{level}: {message}"
+                            self.added += 1
+                            self.remaining += 1
+                        else:
+                            level = data[i]['log_level']
+                            message = data[i]["log_message"]
+                            self.data_stored += f", {level}: {message}"
+                            self.added += 1
+                            self.remaining += 1
+                        i += 1
+            else:
+                raise ValueError("Improper log data")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+def main():
+    """"""
+    print("=== Code Nexus - Data Processor ===\n")
+    numeric = NumericProcessor()
+    text = TextProcessor()
+    log = LogProcessor()
+    bad_tester = [42, "Hello"]
+    n_good_tester = [1, 2, 3, 4, 5]
+    foo = "foo"
+    t_tester = ['Hello', 'Nexus', 'World']
+    l_tester = [{'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+                {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}]
+    # numeric processor
+    print("Testing Numeric Processor...")
+    for d in bad_tester:
+        print(f" Trying to validate '{d}':", numeric.validate(d))
+    print(" Test invalid ingestion of string 'foo' without prior validation:")
+    try:
+        numeric.ingest(foo)
+    except ValueError as e:
+        print(e)
+    print(f" Processing data: {n_good_tester}")
+    print(" Extracting 3 values...")
+    numeric.ingest(n_good_tester)
+    for _ in range(3):
+        n_out_x, n_out_y = numeric.output()
+        print(f" Numeric value {n_out_x}: {n_out_y}")
+    # text processor
+    print("\nTesting Text Processor...")
+    print(f" Trying to validate '42':", text.validate(bad_tester[0]))
+    print(f" Processing data: {t_tester}")
+    text.ingest(t_tester)
+    print(" Extracting 1 value...")
+    text.output
+    t_out_x, t_out_y = text.output()
+    print(f" Text value {t_out_x}: {t_out_y}")
+    # log processor
+    print("\nTesting Log Processor...")
+    print(f" Trying to validate 'Hello':", log.validate(bad_tester[1]))
+    print(f" Processing data: {l_tester}")
+    log.ingest(l_tester)
+    print(" Extracting 2 values...")
+    for _ in range(2):
+        l_out_x, l_out_y = log.output()
+        print(f" Log entry: {l_out_x}: {l_out_y}")
+
+
+main()
---
+++ m05/ex2/data_pipeline.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python3
+# importar esto del futuro porque tengo python 8 y no permite typing.list[]
+from __future__ import annotations
+from abc import ABC, abstractmethod
+import typing
+
+class ExportPlugin(typing.Protocol):
+    pass
+
+    def process_output(self, data: list[tuple[int, str]]) -> None:
+        """"""
+        pass
+
+class ExportCSV(ExportPlugin):
+    pass
+
+    def process_output(self, data: list[tuple[int, str]]) -> None:
+        """"""
+        print("CSV Output:")
+        info_csv = [value for rank, value in data]
+        final_csv = ",".join(info_csv)
+        print(final_csv)
+
+class ExportJSON(ExportPlugin):
+    pass
+
+    def process_output(self, data: list[tuple[int, str]]) -> None:
+        """"""
+        info_json = [{"rank": {rank}, "value": {value}} for rank, value in data] 
+        final_json = "[" + ", ".join(info_json) + "]"
+        print(final_json)
+
+
+class DataProcessor(ABC):
+    pass
+
+    @abstractmethod
+    def validate(self, data: typing.Any) -> bool:
+        """"""
+
+    @abstractmethod
+    def ingest(self, data: typing.Any) -> None:
+        """"""
+
+    def output(self) -> typing.Tuple[int, str]:
+        """"""
+        x = self.rank
+        self.remaining = self.added
+        i = 0
+        while i < len(self.data_stored) and self.data_stored[i] != ",":
+            i += 1
+        y = self.data_stored[:i]
+        self.data_stored = self.data_stored[i + 2:]
+        self.remaining -= 1
+        self.rank += 1
+        out = x, y
+        return out
+
+
+class NumericProcessor(DataProcessor):
+    def __init__(self): 
+        """"""
+        self.data_stored = ""
+        self.rank = 0
+        self.added = 0
+        self.remaining = 0
+
+    def validate(self, data: typing.Any) -> bool:
+        """"""
+        value = False
+        if isinstance(data, (int, float)):
+            value = True
+            return value
+        elif isinstance(data, (list)):
+            return all(isinstance(d, (int, float)) for d in data)
+        return value
+
+    def ingest(self, data: typing.Union[int, float, list]) -> None:
+        """"""
+        try:
+            if self.validate(data):
+                if isinstance(data, (int, float)):
+                    self.data_stored += str(data)
+                    self.added += 1
+                    self.remaining += 1
+                elif isinstance(data, list):
+                    for d in data:
+                        if not self.data_stored:
+                            self.data_stored += str(d)
+                            self.added += 1
+                            self.remaining += 1
+                        else:
+                            self.data_stored += ", " + str(d)
+                            self.added += 1
+                            self.remaining += 1
+            else:
+                raise ValueError("Improper numeric data")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+class TextProcessor(DataProcessor):
+    def __init__(self): 
+        """"""
+        self.data_stored = ""
+        self.rank = 0
+        self.remaining = 0
+        self.added = 0
+
+    def validate(self, data: typing.Any) -> bool:
+        """"""
+        value = False
+        if isinstance(data, (str)):
+            value = True
+            return value
+        elif isinstance(data, (list)):
+            return all(isinstance(d, (str)) for d in data)
+        return value
+    
+    def ingest(self, data: typing.Union[str, list]) -> None:
+        """"""
+        try:
+            if self.validate(data):
+                if isinstance(data, (str)):
+                    self.data_stored += data
+                    self.added += 1
+                    self.remaining += 1
+                elif isinstance(data, list):
+                    for d in data:
+                        if not self.data_stored:
+                            self.data_stored += d
+                            self.added += 1
+                            self.remaining += 1
+                        else:
+                            self.data_stored += ", " + d
+                            self.added += 1
+                            self.remaining += 1
+            else:
+                raise ValueError("Improper string data")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+class LogProcessor(DataProcessor):
+    def __init__(self): 
+        """"""
+        self.data_stored = ""
+        self.rank = 0
+        self.remaining = 0
+        self.added = 0
+
+    def validate(self, data: typing.Any) -> bool:
+        """"""
+        value = False
+        if isinstance(data, (typing.Dict)):
+            value = True
+            return value
+        elif isinstance(data, (list)):
+            return all(isinstance(d, (typing.Dict)) for d in data)
+        return value
+    
+    def ingest(self, data: typing.Union[typing.Dict, list]) -> None:
+        """"""
+        try:
+            if self.validate(data):
+                if isinstance(data, (typing.Dict)):
+                    level = data['log_level']
+                    message = data["log_message"]
+                    self.data_stored += f"{level}: {message}"
+                    self.added += 1
+                    self.remaining += 1
+                elif isinstance(data, list):
+                    i = 0
+                    for d in data:
+                        if not self.data_stored:
+                            level = data[i]['log_level']
+                            message = data[i]["log_message"]
+                            self.data_stored += f"{level}: {message}"
+                            self.added += 1
+                            self.remaining += 1
+                        else:
+                            level = data[i]['log_level']
+                            message = data[i]["log_message"]
+                            self.data_stored += f", {level}: {message}"
+                            self.added += 1
+                            self.remaining += 1
+                        i += 1
+            else:
+                raise ValueError("Improper log data")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+class DataStream:
+    def __init__(self) -> None:
+        self.processors = []
+
+
+    def register_processor(self, proc: DataProcessor) -> None:
+        """"""
+        if proc not in self.processors:
+            self.processors.append(proc)
+
+    def process_stream(self, stream: list[typing.Any]) -> None:
+        """"""
+        for d in stream:
+            accepted = False
+            for proc in self.processors:
+                if proc.validate(d):
+                    proc.ingest(d)
+                    accepted = True
+            if not accepted:
+                print("DataStreamError - "
+                      f"Can't process element in stream: {d}")
+
+    def print_processors_stats(self) -> None:
+        """"""
+        print("== DataStream statistics ==")
+        if len(self.processors) == 0:
+                print("No processor found, no data")
+                return
+        else:
+            for proc in self.processors:
+                name = type(proc).__name__.replace("Processor", " Processor")
+                total = proc.added
+                remaining = proc.remaining
+                print(f"{name}: total {total} items processed, remaining {remaining} on processor")
+    
+    def output_pipeline(self, nb: int, plugin: ExportPlugin) -> None:
+        """"""
+        pipeline = []
+        for proc in self.processors:
+            count = 0
+            while count < nb and proc.data_stored != "":
+                save_tuple = proc.output()
+                pipeline.append(save_tuple)
+                count += 1
+        plugin.process_output(pipeline)
+
+
+
+
+
+def main():
+    """"""
+    print("=== Code Nexus - Data Stream ===\n")
+    numeric = NumericProcessor()
+    text = TextProcessor()
+    log = LogProcessor()
+    plug_csv = ExportCSV()
+    plug_json = ExportJSON()
+    first_batch = [
+        'Hello world', [3.14, -1, 2.71],
+        [
+            {'log_level': 'WARNING',
+             'log_message': 'Telnet access! Use ssh instead'},
+            {'log_level': 'INFO',
+             'log_message': 'User wil isconnected'}
+            ],
+         42,
+         ['Hi', 'five']
+         ]
+    data_stream = DataStream()
+    # no processors
+    print("Initialize Data Stream...\n")
+    # no data
+    data_stream.print_processors_stats()
+    print("\nRegistering Processors\n")
+    data_stream.register_processor(numeric)
+    data_stream.register_processor(text)
+    data_stream.register_processor(log)
+    print(f"Send first batch of data on stream: {first_batch}\n")
+    data_stream.process_stream(first_batch)
+    data_stream.print_processors_stats()
+    # send to csv plugin
+    print(numeric.data_stored)
+    print("\nSend 3 processed data from each processor to a CSV plugin:")
+    for proc in data_stream.processors
+    data_stream.output_pipeline(3, plug_csv)
+
+    
+
+
+main()
+
+
+# tengo que hacer al cuenta de remaining cuando se añaden los datos que si no no se suma

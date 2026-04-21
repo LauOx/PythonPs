ccdb9e81baba90fd477bb6d9411967776bf5ec08
LauOx <laulefu@gmail.com>
2026-04-21 20:46:25 +0200

    m08 ex0 terminado

---
+++ m08/ex0/construct.py
@@ -0,0 +1,43 @@
+#!/bin/usr/env python3
+
+import sys
+import os
+import site
+
+def detect_environment()-> bool:
+    """"""
+    if os.getenv('VIRTUAL_ENV'):
+        return True
+    return False
+
+
+def env_status() -> None:
+    """"""
+    if not detect_environment():
+        print("\nMATRIX STATUS: You're still plugged in\n")
+        print("Current python:", sys.executable)
+        print("Virtual Environment: None detected\n")
+        print("WARNING: You're in the global environment! "
+              "\nThe machines can see everything you install.\n")
+        print("To enter the construct, run:\n"
+              "python -m venv matrix_env\n"
+              "source matrix_env/bin/activate # On Unix\n"
+              r"matrix_env\Scripts\activate # On Windows")
+        print("\nThen run this program again.")
+    else:
+        exe = sys.executable
+        e_path = os.getenv('VIRTUAL_ENV')
+        v_env = os.path.basename(e_path)
+        f_pack = site.getsitepackages()
+        print("\nMATRIX STATUS: Welcome to the construct\n")
+        print("Current python:", exe)
+        print("Virtual Environment:", v_env)
+        print("Environment Path:", e_path)
+        print("\nSUCCESS: You're in an isolated environment!\n"
+              "Safe to install packages without affecting\n"
+              "the global system.")
+        print("Package installation path:", f_pack[0])
+
+
+
+env_status()
